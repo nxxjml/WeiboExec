376eb7c9c561de0c6dbd9dbf973dd7a86d7ea9aa
nxxjml <nxxjml@sina.cn>
2015-09-09 01:58:29 -0700

    yrdy

---
+++ WeiboExec/WeiboExec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiboExec
+//
+//  Created by simao on 15/9/9.
+//  Copyright (c) 2015年 simao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeiboExec/WeiboExec/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeiboExec
+//
+//  Created by simao on 15/9/9.
+//  Copyright (c) 2015年 simao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
