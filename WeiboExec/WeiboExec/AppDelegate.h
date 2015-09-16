3070ef433129edf456785ee1aa9a3190684e314b
nxxjml <nxxjml@sina.cn>
2015-09-16 02:36:10 -0700

    Success

--- WeiboExec/WeiboExec/AppDelegate.h
+++ WeiboExec/WeiboExec/AppDelegate.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
+#import "SendMessageToWeiboViewController.h"
 
-@class SendMessageToWeiboViewController;
+//@class SendMessageToWeiboViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- WeiboExec/WeiboExec/SendMessageToWeiboViewController.h
+++ WeiboExec/WeiboExec/SendMessageToWeiboViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SendMessageToWeiboViewController : NSObject
+@interface SendMessageToWeiboViewController : UIViewController
 
 @end
